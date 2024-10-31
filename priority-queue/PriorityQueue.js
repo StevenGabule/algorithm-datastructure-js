95b9b5b0db2580a1035918b5d5e350d6b2f8b009
John Paul Gabule <lucas.gabule@gmail.com>
2024-10-31 14:31:01 +0800

    completed priority queue

--- priority-queue/PriorityQueue.js
+++ priority-queue/PriorityQueue.js
@@ -1,3 +1,177 @@
-class PriorityQueue extends MinHeap{
+class Comparator {
+  /**
+   * Constructor.
+   * @param {function(a: *, b: *)} [compareFunction] - It may be custom compare function that, let's
+   * say may compare custom objects together.
+   */
+  constructor(compareFunction) {
+    this.compare = compareFunction || Comparator.defaultCompareFunction;
+  }
 
+  /**
+   * Default comparison function. It just assumes that "a" and "b" are strings or numbers.
+   * @param {(string|number)} a
+   * @param {(string|number)} b
+   * @returns {number}
+   */
+  static defaultCompareFunction(a, b) {
+    if (a === b) {
+      return 0;
+    }
+
+    return a < b ? -1 : 1;
+  }
+
+  /**
+   * Checks if two variables are equal.
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  equal(a, b) {
+    return this.compare(a, b) === 0;
+  }
+
+  /**
+   * Checks if variable "a" is less than "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  lessThan(a, b) {
+    return this.compare(a, b) < 0;
+  }
+
+  /**
+   * Checks if variable "a" is greater than "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  greaterThan(a, b) {
+    return this.compare(a, b) > 0;
+  }
+
+  /**
+   * Checks if variable "a" is less than or equal to "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  lessThanOrEqual(a, b) {
+    return this.lessThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * Checks if variable "a" is greater than or equal to "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  greaterThanOrEqual(a, b) {
+    return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * Reverses the comparison order.
+   */
+  reverse() {
+    const compareOriginal = this.compare;
+    this.compare = (a, b) => compareOriginal(b, a);
+  }
+}
+
+class PriorityQueue extends MinHeap {
+  constructor() {
+    // Call MinHip constructor first.
+    super();
+
+    // Setup priorities map.
+    this.priorities = new Map();
+
+    // Use custom comparator for heap elements that will take element priority
+    // instead of element value into account.
+    this.compare = new Comparator(this.comparePriority.bind(this));
+  }
+
+
+  /**
+   * Add item to the priority queue.
+   * @param {*} item - item we're going to add to the queue.
+   * @param {number} [priority] - items priority.
+   * @return {PriorityQueue}
+   */
+  add(item, priority = 0) {
+    this.priorities.set(item, priority);
+    super.add(item);
+    return this;
+  }
+
+  /**
+   * Remove item from priority queue.
+   * @param {*} item - item we're going to remove.
+   * @param {Comparator} [customFindingComparator] - custom function for finding the item to remove
+   * @return {PriorityQueue}
+   */
+  remove(item, customFindingComparator) {
+    super.remove(item, customFindingComparator);
+    this.priorities.delete(item);
+    return this;
+  }
+
+  /**
+   * Change priority of the item in a queue.
+   * @param {*} item - item we're going to re-prioritize.
+   * @param {number} priority - new item's priority.
+   * @return {PriorityQueue}
+   */
+  changePriority(item, priority) {
+    this.remove(item, new Comparator(this.compareValue));
+    this.add(item, priority);
+    return this;
+  }
+
+  /**
+   * Find item by ite value.
+   * @param {*} item
+   * @return {Number[]}
+   */
+  findByValue(item) {
+    return this.find(item, new Comparator(this.compareValue));
+  }
+
+  /**
+   * Check if item already exists in a queue.
+   * @param {*} item
+   * @return {boolean}
+   */
+  hasValue(item) {
+    return this.findByValue(item).length > 0;
+  }
+
+  /**
+   * Compares priorities of two items.
+   * @param {*} a
+   * @param {*} b
+   * @return {number}
+   */
+  comparePriority(a, b) {
+    if (this.priorities.get(a) === this.priorities.get(b)) {
+      return 0;
+    }
+    return this.priorities.get(a) < this.priorities.get(b) ? -1 : 1;
+  }
+
+  /**
+   * Compares values of two items.
+   * @param {*} a
+   * @param {*} b
+   * @return {number}
+   */
+  compareValue(a, b) {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  }
 }
